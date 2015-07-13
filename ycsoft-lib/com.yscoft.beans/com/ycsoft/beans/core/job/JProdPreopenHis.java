fc1674c31851c223d23cb00651371c010efef7d1
Killer <leopardoooo@163.com>
2015-07-13 21:21:54 +0800

    lib模块修改为标准的maven结构

--- ycsoft-lib/com.yscoft.beans/com/ycsoft/beans/core/job/JProdPreopenHis.java
+++
@@ -1,8 +0,0 @@
-package com.ycsoft.beans.core.job;
-
-import com.ycsoft.daos.config.POJO;
-
-@POJO(tn="J_PROD_PREOPEN_HIS",sn = "", pk = "")
-public class JProdPreopenHis extends JProdPreopen{
-	
-}
