9effa06e0813108cd077b47bd96b1a6ff575c734
panyuben <pyblovesyl@163.com>
2015-07-21 08:43:22 +0700

    移除已提交的本地Log文件

--- boss-remoting/src/test/java/com/ycsoft/boss/remoting/ott/OttClientTest.java
+++ boss-remoting/src/test/java/com/ycsoft/boss/remoting/ott/OttClientTest.java
@@ -7,25 +7,25 @@ import org.junit.Test;
 public class OttClientTest {
 	OttClient client = new OttClient();
 
-	//@Test
+	@Test
 	public void testCreateUser() {
 		Result result = client.createUser(10000001, "123456", "ptest1", "moniwang", null, null, null, null);
 		assertEquals("0",result.getErr());
 	}
 	
-	//@Test
+	@Test
 	public void testDeleteUser() {
 		Result result = client.deleteUser(99032342);
 		assertEquals("0", result.getErr());
 	}
 	
-	//@Test
+	@Test
 	public void addProduct() {
-		Result result = client.addOrUpdateProduct("12345321", "ÖĞÎÄÆµµÀ");
+		Result result = client.addOrUpdateProduct("12345321", "å¥½äº§å“å•Š");
 		assertEquals("0", result.getErr());
 	}
 	
-	//@Test
+	@Test
 	public void testDeleteProduct() {
 		Result result = client.deleteProduct("12345321");
 		assertEquals("0", result.getErr());
