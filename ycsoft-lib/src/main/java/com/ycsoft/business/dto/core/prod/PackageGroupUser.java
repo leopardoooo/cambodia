db88ef9bd10ad006f73ba1da8846b26c9db2e73d
sunrise1203 <10251882@163.com>
2015-07-30 16:42:04 +0800

    套餐修改设计

--- ycsoft-lib/src/main/java/com/ycsoft/business/dto/core/prod/PackageGroupUser.java
+++ ycsoft-lib/src/main/java/com/ycsoft/business/dto/core/prod/PackageGroupUser.java
@@ -2,10 +2,8 @@ package com.ycsoft.business.dto.core.prod;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 import com.ycsoft.beans.prod.PPackageProd;
-import com.ycsoft.beans.prod.PPackageProdDetail;
 import com.ycsoft.beans.prod.PProd;
 
 public class PackageGroupUser implements Serializable  {
@@ -13,14 +11,20 @@ public class PackageGroupUser implements Serializable  {
 	//套餐内容组ID
 	private String package_group_id;
     //内容组选中的终端用户id数组
-    private List<String>    userSelectList;
-	//产品组选中的产品：Map<packege_prod_content_id,List<prod_id>>
-    private Map<String,List<String>>    prodSelectMap;
-   
+    private List<String>  userSelectList;
     //套餐内容组定义
     private PPackageProd pPackageProd;
-    //套餐内容产品组定义和prod_list对应的所有产品信息
-    private Map<PPackageProdDetail,List<PProd>> prodcontentMap;
+	//套餐内容组中产品内容清单
+    private List<PProd>  prodList;
+
+    
+    public List<PProd> getProdList() {
+		return prodList;
+	}
+	public void setProdList(List<PProd> prodList) {
+		this.prodList = prodList;
+	}
+
 	public String getPackage_group_id() {
 		return package_group_id;
 	}
@@ -33,24 +37,12 @@ public class PackageGroupUser implements Serializable  {
 	public void setUserSelectList(List<String> userSelectList) {
 		this.userSelectList = userSelectList;
 	}
-	public Map<String, List<String>> getProdSelectMap() {
-		return prodSelectMap;
-	}
-	public void setProdSelectMap(Map<String, List<String>> prodSelectMap) {
-		this.prodSelectMap = prodSelectMap;
-	}
+	
 	public PPackageProd getpPackageProd() {
 		return pPackageProd;
 	}
 	public void setpPackageProd(PPackageProd pPackageProd) {
 		this.pPackageProd = pPackageProd;
-	}
-	public Map<PPackageProdDetail, List<PProd>> getProdcontentMap() {
-		return prodcontentMap;
-	}
-	public void setProdcontentMap(
-			Map<PPackageProdDetail, List<PProd>> prodcontentMap) {
-		this.prodcontentMap = prodcontentMap;
 	}
     
 }
