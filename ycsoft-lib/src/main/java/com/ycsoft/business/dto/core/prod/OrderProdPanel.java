3f175fc8aa44a5c817ff32cb3eaa29b16532441b
panyuben <pyblovesyl@163.com>
2015-07-31 10:16:34 +0700

    注释乱码

--- ycsoft-lib/src/main/java/com/ycsoft/business/dto/core/prod/OrderProdPanel.java
+++ ycsoft-lib/src/main/java/com/ycsoft/business/dto/core/prod/OrderProdPanel.java
@@ -1,5 +1,7 @@
 package com.ycsoft.business.dto.core.prod;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,16 +16,20 @@ import com.ycsoft.beans.prod.PProdTariffDisct;
 public class OrderProdPanel {
 	/**
 	 * 用户栏目描述
-	 *  如果是单产品订购情况，返回 用户类型+设备编号 (DTT_12312312,OTT_122)
-	 * 如果是套餐续订或升级情况，返回 套餐的内容组用户选择描述( 例如： OTT一组=8，宽带组=2，OTT2组=4）
-　　	 * 其他情况，返回空字符串
+	 *  如果是单产品订购情况，返用户类型+设备编号 (DTT_12312312,OTT_122)
+?	 * 其他情况，返回空字符
 	 */
 	private String userDesc;
-	//可用产品组
+	//可用产品
 	private List<PProd> prodList;
-	//可用资费组 Map<prod_id,List<资费折扣定义>>
+	//可用资费Map<prod_id,List<资费折扣定义>>
 	private Map<String,List<PProdTariffDisct>> tariffMap;
-	//上期订购记录Map<prod_id,订购记录>
+	/**上期订购记录Map<prod_id,订购记录>
+	 * 情况1 升级：上期订购记录是选中的要升级的订
+	 * 情况2非升级： a.非宽带单产品的上期订购记录是 exp_date>=今天，相同user_id,相同prod_id(含套餐的子产 exp_date的订购记录
+	 *           b.宽带单产品的上期订购记录exp_date>=今天，相同user_id,prod_id in(宽带单产品，含套餐的子产) exp_date的订购记
+	 *           c.套餐的上期订购记录是   exp_date>=今天，相同cust_id,prod_id=套餐的exp_date的订购记录
+	**/
 	private Map<String,CProdOrder> lastOrderMap;
 	
 	public String getUserDesc() {
@@ -50,6 +56,14 @@ public class OrderProdPanel {
 	public void setLastOrderMap(Map<String, CProdOrder> lastOrderMap) {
 		this.lastOrderMap = lastOrderMap;
 	}
+	public OrderProdPanel() {
+		prodList = new ArrayList<>();
+		tariffMap = new HashMap<>();
+		lastOrderMap = new HashMap<>();
+		
+	}
+	
+	
 	
 	
 }
