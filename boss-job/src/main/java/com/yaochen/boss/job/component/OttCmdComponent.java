3d247278d52fd95a5f655b43311654375483631a
panyuben <pyblovesyl@163.com>
2015-08-06 09:17:38 +0700

    授权接口

---
+++ boss-job/src/main/java/com/yaochen/boss/job/component/OttCmdComponent.java
@@ -0,0 +1,8 @@
+package com.yaochen.boss.job.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OttCmdComponent {
+
+}
---
+++ ycsoft-lib/src/main/java/com/ycsoft/beans/core/user/UserResExpDate.java
@@ -0,0 +1,23 @@
+package com.ycsoft.beans.core.user;
+
+import java.util.Date;
+
+public class UserResExpDate {
+	private String res_id;
+	private Date expDate;
+	public String getRes_id() {
+		return res_id;
+	}
+	public void setRes_id(String res_id) {
+		this.res_id = res_id;
+	}
+	public Date getExpDate() {
+		return expDate;
+	}
+	public void setExpDate(Date expDate) {
+		this.expDate = expDate;
+	}
+	
+	
+
+}
