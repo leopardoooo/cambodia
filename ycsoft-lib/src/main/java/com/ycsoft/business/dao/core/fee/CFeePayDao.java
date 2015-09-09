4d2af4970380b57466ed79a630f9a2b147b3025a
sunrise1203 <10251882@163.com>
2015-09-09 17:10:32 +0800

    1.发票作废处理，插入未打印信息，限制只有开票人可以作废 2.按客户名搜索客户后台sql优化

--- ycsoft-lib/src/main/java/com/ycsoft/business/dao/core/fee/CFeePayDao.java
+++ ycsoft-lib/src/main/java/com/ycsoft/business/dao/core/fee/CFeePayDao.java
@@ -210,7 +210,6 @@ public class CFeePayDao extends BaseEntityDao<CFeePay> {
 				"    AND T1.cust_id=? ",
 				"    AND T1.STATUS = ? AND t1.pay_type=t5.pay_type and t5.is_print='T'",
 				"    AND T1.is_doc= ? ",
-				"    AND T2.FEE_TYPE IN (?,?)",
 				"    AND T1.optr_id= ? ",
 				" UNION ALL ",
 				" SELECT T1.FEE_SN, T1.REAL_PAY AMOUNT, T2.PRINTITEM_ID,t3.doc_type,t1.acct_id,'' prom_fee_sn",
@@ -226,8 +225,7 @@ public class CFeePayDao extends BaseEntityDao<CFeePay> {
 		
 		return findToList(sql, 
 				countyId, SystemConstants.TEMPLATE_TYPE_INVOICE, countyId,custId,
-				StatusConstants.PAY, SystemConstants.BOOLEAN_FALSE,SystemConstants.FEE_TYPE_BUSI,
-				SystemConstants.FEE_TYPE_DEVICE,optrId,
+				StatusConstants.PAY, SystemConstants.BOOLEAN_FALSE,optrId,
 				countyId,SystemConstants.TEMPLATE_TYPE_INVOICE, countyId,custId,
 				 StatusConstants.PAY, SystemConstants.BOOLEAN_FALSE, SystemConstants.FEE_TYPE_ACCT,optrId
 				);
