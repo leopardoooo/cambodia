642a81cfc5310ed85b62e1b925cece1230d94a28
jpdan <danjp@163.com>
2015-09-23 20:25:15 +0800

    设备查询 账号和设备编号查询放一起

--- boss-login/src/main/webapp/i18n/zh/boss-core-lang.js
+++ boss-login/src/main/webapp/i18n/zh/boss-core-lang.js
@@ -71,7 +71,7 @@ BCLang.common = {
 //主页模块
 BCLang.home = {
 	topWelcome: "当前操作员~",
-	searchTabs:["名称编号~","设备编号~","安装地址~","电话号码~","多条件搜索~"],
+	searchTabs:["名称编号~","设备|账号~","安装地址~","电话号码~","多条件搜索~"],
 	invoicePrintTitle:'收据打印~',
 	searchTip:"客户编号|受理编号~",
 	searchBtns:["搜索~","缴费~"],
