39a135361639cef10b5bcec7de65ac86018bf465
jpdan <danjp@163.com>
2015-09-30 18:10:11 +0800

    Signed-off-by: jpdan <danjp@163.com>

--- ycsoft-lib/src/main/java/com/ycsoft/business/dao/resource/device/RDeviceDao.java
+++ ycsoft-lib/src/main/java/com/ycsoft/business/dao/resource/device/RDeviceDao.java
@@ -936,4 +936,19 @@ public class RDeviceDao extends BaseEntityDao<RDevice> {
 				+ " from r_stb_model t order by model_name ";
 		return this.createQuery(RDeviceModel.class, sql ).list();
 	}
+	
+	public List<RDevice> queryDeviceInfoByDoneCode(Integer deviceDoneCode) throws Exception {
+		String sql = "select sm.model_name,ds.supplier_name,d.* from r_device_done_deviceid di, r_device d, r_stb_model sm, r_device_supplier ds"
+				+ " where di.device_id=d.device_id and d.device_model=sm.device_model and sm.supplier_id=ds.supplier_id(+)"
+				+ " and di.device_done_code=?"
+				+ " union all"
+				+ " select sm.model_name,ds.supplier_name,d.* from r_device_done_deviceid di, r_device d, r_modem_model sm, r_device_supplier ds"
+				+ " where di.device_id=d.device_id and d.device_model=sm.device_model and sm.supplier_id=ds.supplier_id(+)"
+				+ " and di.device_done_code=?"
+				+ " union all"
+				+ " select sm.model_name,ds.supplier_name,d.* from r_device_done_deviceid di, r_device d, r_device_model sm, r_device_supplier ds"
+				+ " where di.device_id=d.device_id and d.device_model=sm.device_model and sm.supplier_id=ds.supplier_id(+)"
+				+ " and di.device_done_code=?";
+		return this.createQuery(sql, deviceDoneCode, deviceDoneCode, deviceDoneCode).list();
+	}
 }
