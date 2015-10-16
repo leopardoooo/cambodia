bcec64f1d0e6f4d1b90a29d24bd9a598ad610142
Killer <leopardoooo@163.com>
2015-10-16 16:58:48 +0800

    证书
    Signed-off-by: Killer <leopardoooo@163.com>

--- boss-remoting/src/test/java/com/ycsoft/boss/remoting/cfocn/SSLTest.java
+++ boss-remoting/src/test/java/com/ycsoft/boss/remoting/cfocn/SSLTest.java
@@ -19,7 +19,7 @@ public class SSLTest {
         System.setProperty("javax.net.ssl.keyStorePassword", "a1234567");
         System.setProperty("javax.net.ssl.keyStoreType", "PKCS12");
 		
-		System.setProperty("javax.net.ssl.trustStore", "E:/MyWork/workspace_st/cambodia/boss-core/src/main/resources/cert/cfocn/cfocn.ca.jks");
+		System.setProperty("javax.net.ssl.trustStore", "/Users/killer/Documents/MyWorks/GitHub/cambodia/boss-core/src/main/resources/cert/cfocn/cfocn.ca.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "a1234567");
         System.setProperty("javax.net.ssl.trustStoreType", "JKS");
 		
